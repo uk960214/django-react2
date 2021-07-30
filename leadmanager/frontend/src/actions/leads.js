6bb1366d51225838e13adee63e39923eeaaf7cbe
koowoy <uk960214@gmail.com>
2021-07-30 23:31:36 +0900

    complete error handling, alerts

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { createMessage } from "./messages";
 
-import { GET_LEADS, DELETE_LEADS, ADD_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEADS, ADD_LEAD, GET_ERRORS } from "./types";
 
 // GET LEADS
 export const getLeads = () => (dispatch) => {
@@ -20,6 +21,7 @@ export const deleteLeads = (id) => (dispatch) => {
   axios
     .delete(`/api/leads/${id}`)
     .then((res) => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({
         type: DELETE_LEADS,
         payload: id,
@@ -33,10 +35,20 @@ export const addLead = (lead) => (dispatch) => {
   axios
     .post("/api/leads/", lead)
     .then((res) => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({
         type: ADD_LEAD,
         payload: res.data,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const errors = {
+        msg: err.response.data,
+        status: err.response.status,
+      };
+      dispatch({
+        type: GET_ERRORS,
+        payload: errors,
+      });
+    });
 };
